d6e68898bde405acea82c4fa214ececa8561e253
Mehta Aum <maum5302@gmail.com>
2025-09-23 18:53:35 +0530

    file system(writeFile, appendFile, rename, copyFile)

---
+++ Day 01/script.js
@@ -0,0 +1,57 @@
+/**
+ * -> Node.js File System (fs) Module - Quick Notes
+ * 
+ * - fs.writeFile(path, data, callback) → Creates a file OR overwrites existing file.
+ * - fs.appendFile(path, data, callback) → Appends data to a file (creates if not exist).
+ * - fs.rename(oldPath, newPath, callback) → Renames or moves a file.
+ * - fs.copyFile(src, dest, callback) → Copies content of one file to another.
+ * 
+ * -> These are asynchronous methods → they don’t run in strict order.
+ *    Always handle errors inside callbacks.
+ */
+
+
+const fs = require('fs')
+
+fs.writeFile("hello.txt" , "kem cho", (err) => {
+    if (err) {
+        console.log(err);
+    }
+    else{
+        console.log("writeFile done");
+        
+    }
+})
+
+
+fs.appendFile("hello.txt" , " badha maja ma ne mota", (err) => {
+    if (err) {
+        console.log(err);
+    }
+    else{
+        console.log("append done");
+        
+    }
+})
+
+fs.rename("hello.txt" , "hey.txt" , (err) => {
+    if (err) {
+        console.log(err);
+    }
+    else{
+        console.log("rename done");
+        
+    }
+})
+
+fs.copyFile("hey.txt" , "./copy/copy.txt" , (err) => {
+    if (err) {
+        console.log(err.message);
+    }
+    else{
+        console.log("copyFile done");
+        
+    }
+})
+
+
