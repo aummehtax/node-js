b36b44f9be2b326ca39db2b7c85d0dd69e7f6ee3
Mehta Aum <maum5302@gmail.com>
2025-09-24 19:46:44 +0530

    instal.. & unin..(basic & adv), node-modules, dep & dev-dep, scripts & custom-scripts

---
+++ Day 04/installing-and-uninstalling-anything-basics-and-advanced/script.js
@@ -0,0 +1,3 @@
+//install -> npm i packageKaNaam
+//uninstall -> npm uninstall packageKaNaam
+//installing particular version  example (npm i axios@1.7.8)
---
+++ Day 04/scripts-notes/script.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+
+console.log(chalk.redBright("hello world")); 
+console.log(chalk.bgMagenta("hello world")); // just exploring some packages
