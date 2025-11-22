8223c914b406bea0ce64d9732a6e392f14dc9d72
Mehta Aum <maum5302@gmail.com>
2025-11-22 15:21:46 +0530

    professional backend (add routes change password, get current user, update account details, update avatar, update cover image, get user channel profile, and watch history)

--- Day 16/src/routes/user.routes.js
+++ Day 16/src/routes/user.routes.js
@@ -1,7 +1,7 @@
 // Express Router → helps organize routes into separate files/modules for cleaner, modular code
 
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,5 +24,13 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT , changeCurrentPassword)
+router.route("/current-user").get(verifyJWT , getCurrentUser)
+router.route("/update-account-details").patch(verifyJWT , updateAccountDetails)
+router.route("/avatar").patch(verifyJWT , upload.single("avatar") , updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT , upload.single("coverImage") , updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT , getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT , getWatchHistory)
 
+ 
 export default router
